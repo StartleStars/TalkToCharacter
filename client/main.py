0637d8487841332acd3ce844128ba965f473cfb4
StartleStars <36647583+StartleStars@users.noreply.github.com>
2019-11-29 20:06:12 +0000

    Add files via upload

--- client/main.py
+++ client/main.py
@@ -38,3 +38,20 @@ print(_("CPU Checked"))
 #Check system RAM
 #INCOMPLETE
 
+#Load information on available characters
+#INCOMPLETE
+
+#Initialize Kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MainApp(App):
+    def build(self):
+        label = Label(text=_("Hello from Kivy"),
+                      size_hint=(.5, .5),
+                      pos_hint={'center_x': .5, 'center_y': .5})
+        return label
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
