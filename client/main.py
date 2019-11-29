96c932eab26a6336949b4a28a0fd8f4c13fb4d09
StartleStars <36647583+StartleStars@users.noreply.github.com>
2019-11-29 23:52:26 +0000

    Main Menu layout

--- client/main.py
+++ client/main.py
@@ -38,20 +38,101 @@ print(_("CPU Checked"))
 #Check system RAM
 #INCOMPLETE
 
-#Load information on available characters
-#INCOMPLETE
+#Find characters in directory
+import os
+availablecharacters = []
+for file in os.listdir("./characters"):
+    if file.endswith(".ini"):
+        availablecharacters.append(file)
 
 #Initialize Kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
 
-class MainApp(App):
+class MainMenu(App):
     def build(self):
-        label = Label(text=_("Hello from Kivy"),
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
-        return label
+        layout = BoxLayout(padding=10,
+                           orientation = 'horizontal')
+        #build the left side
+        left_side = BoxLayout(padding=10,
+                              orientation = 'vertical',
+                              size_hint = (.3, 1))
+        layout.add_widget(left_side)
+        #build the left hand side's top bar
+        left_side_top = BoxLayout(size_hint = (1, .1))
+        left_side.add_widget(left_side_top)
+        top_button = Button(text=_('Menu'),
+                            background_color = [1,0,0,1],
+                            size_hint = (0.2,1))
+        left_side_top.add_widget(top_button)
+        top_label = Label(text=_('TalkToCharacter'),
+                          size_hint = (0.8,1))
+        left_side_top.add_widget(top_label)
+        #build the character select
+        character_selection = Label(text=_('Character Selection'),
+                                    size_hint=(1, .7))
+        left_side.add_widget(character_selection)
+        #build the "open character directory" button
+        character_directory = Button(text=_("Open Character Folder"),
+                                     background_color = [1,0,0,1],
+                                     size_hint =(1, .1))
+        left_side.add_widget(character_directory)
+        #build the "download characters" button
+        character_download = Button(text=_("Download More Characters"),
+                                     background_color = [1,0,0,1],
+                                     size_hint =(1, .1))
+        left_side.add_widget(character_download)
+        #build the right side
+        right_side = BoxLayout(padding=10,
+                               orientation = 'vertical',
+                               size_hint = (.7, 1))
+        layout.add_widget(right_side)
+        #build the feed
+        feed_window = Label(text=_('News Feed'),
+                            size_hint = (1, .3))
+        right_side.add_widget(feed_window)
+        #build character info
+        character_info = BoxLayout(padding=10,
+                                   orientation = 'horizontal',
+                                   size_hint = (1, .7))
+        right_side.add_widget(character_info)
+        #build left side of character info
+        character_left = BoxLayout(padding=10,
+                                   orientation = 'vertical',
+                                   size_hint = (.4, 1))
+        character_info.add_widget(character_left)
+        #build image of character
+        character_image = Image(source='./example.png',
+                                size_hint=(1, .4))
+        character_left.add_widget(character_image)
+        #build technical details
+        character_technical = Label(text=_('Technical Details'),
+                                    size_hint=(1,.4))
+        character_left.add_widget(character_technical)
+        #build settings button
+        settings_button = Button(text=_("Settings"),
+                                 background_color = [1,0,0,1],
+                                 size_hint=(1,.2))
+        character_left.add_widget(settings_button)
+        #build right side of character info
+        character_right = BoxLayout(padding=10,
+                                    orientation = 'vertical',
+                                    size_hint = (.6, 1))
+        character_info.add_widget(character_right)
+        #build character bio
+        character_bio = Label(text=_('Character Bio'),
+                              size_hint = (1, .8))
+        character_right.add_widget(character_bio)
+        #build start button
+        start_button = Button(text=_("Start Chat"),
+                              background_color = [1,0,0,1],
+                              size_hint=(1, .2))
+        character_right.add_widget(start_button)
+        return layout
 
 if __name__ == '__main__':
-    app = MainApp()
+    app = MainMenu()
     app.run()
